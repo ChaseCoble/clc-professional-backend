f5f08ed575a0ce536746ca1b6c55cb1fdfde00a1
Chase Mobile <chasecobledevelopment@gmail.com>
2024-08-31 21:34:58 -0500

    Database url change

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 #basedir = os.path.abspath(os.path.dirname(__file__))
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'app.sqlite')
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ufn5c1gf0v1ltq:p71e8ce7d3088ba3fb2739237e4ff7d0fdae3b99f202c415d6f0d2d753bf7ce4a@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dalbsh3n64d8jb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://u8s73brhf81to4:p71309ff76c9a144c3320f62dab39f44d9e8fb92d01af2bd92c6b6276cce413f3@ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d1ujdqnuehl93d"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
